11b55d832833103d3ade7390f49519d8dc3e9b5f
ty-cooper <ty.j.cooper@icloud.com>
2022-03-07 12:53:28 -0800

    Finish template set up for 'scrambled eggs'

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ def home():
 def chicken_salad():
     return render_template("chicken_salad.html")
 
-@app.route("/PBJ")
+@app.route("/pbj")
 def pbj():
     return render_template("PBJ.html")
 
+@app.route("/scrambled-eggs")
+def scrambled_eggs():
+    return render_template("scrambled_eggs.html")
